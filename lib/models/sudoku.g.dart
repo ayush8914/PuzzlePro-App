8c60c9b97f0fcb9a5507158db35608a8843bdd93
dhrumilpatel30 <dhrumilp063@gmail.com>
2024-01-06 12:41:27 +0530

    Add sudoku storage back

---
+++ lib/models/sudoku.g.dart
@@ -0,0 +1,60 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sudoku.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class SudokuAdapter extends TypeAdapter<Sudoku> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Sudoku read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Sudoku(
+      (fields[0] as List).map((dynamic e) => (e as List).cast<int>()).toList(),
+      fields[5] as bool,
+      fields[6] as String,
+    )
+      ..addedDigits = (fields[2] as List?)
+          ?.map((dynamic e) => (e as List).cast<int>())
+          ?.toList()
+      ..lastViewed = fields[3] as DateTime
+      ..isComplete = fields[4] as bool;
+  }
+
+  @override
+  void write(BinaryWriter writer, Sudoku obj) {
+    writer
+      ..writeByte(7)
+      ..writeByte(0)
+      ..write(obj.originalSudoku)
+      ..writeByte(1)
+      ..write(obj.createdAt)
+      ..writeByte(2)
+      ..write(obj.addedDigits)
+      ..writeByte(3)
+      ..write(obj.lastViewed)
+      ..writeByte(4)
+      ..write(obj.isComplete)
+      ..writeByte(5)
+      ..write(obj.isScanned)
+      ..writeByte(6)
+      ..write(obj.difficulty);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SudokuAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
